38c7e28a6355f06b34f73a5359af7ee2fc2b557d
Phan Thi Mai <m001938@cb.local>
2020-09-04 08:34:20 +0700

    Initialize project

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8"/>
+    <title>Users</title>
+</head>
+
+<body>
+<h1>User management</h1>
+
+<h3>1. <a href="Controller/User_controller.php">Show List Users</a></h3>
+
+</body>
+</html>
